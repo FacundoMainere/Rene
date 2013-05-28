15e5bdbf99140f85986eddb1e647a5c7d865e427
Facundo Mainere <fmmainere@gmail.com>
2013-05-28 18:35:24 -0300

    #1 - added step definitions for current  feature

--- features/step_definitions/rene_steps.rb
+++ features/step_definitions/rene_steps.rb
@@ -8,3 +8,35 @@ Then(/^I should see "(.*?)"$/) do |expected|
     assert page.has_content?(expected)
   end
 end
+
+Given(/^I am on "(.*?)"$/) do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I fill in "(.*?)" with "(.*?)"$/) do |arg1, arg2|
+    pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I fill in "(.*?)" with tomorrow$/) do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
+When(/^I press "(.*?)"$/) do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the appointment with "(.*?)" the day "(.*?)" at "(.*?)" with a duration of "(.*?)" minutes was already booked$/) do |arg1, arg2, arg3, arg4|
+    pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I fill in "(.*?)" with yesterday$/) do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I fill in "(.*?)" with today$/) do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I fill in "(.*?)" with a past hour$/) do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
