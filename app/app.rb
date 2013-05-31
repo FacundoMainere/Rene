c2d95c3239c55de343004ba933e383f125e12c26
altamiranoErica <altamirano.erica@gmail.com>
2013-05-31 19:35:11 -0300

    #18 - Change method 'post' to 'get'

--- app/app.rb
+++ app/app.rb
@@ -33,7 +33,7 @@ module Rene
       render '/home/login'
     end
 
-    post :auth, :map => '/auth/:provider/callback' do
+    get :auth, :map => '/auth/:provider/callback' do
       auth = request.env["omniauth.auth"]
       account = Account.find_by_provider_and_uid(auth["provider"], auth["uid"]) ||
                 Account.create_with_omniauth(auth)
