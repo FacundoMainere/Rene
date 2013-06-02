d95db7ab75cc77a81b4d1bbf4ef3afdf9d30fe19
Facundo Mainere <fmmainere@gmail.com>
2013-06-01 21:43:22 -0300

    #13 - add_new_appointment defined in Appointment model, with all(3) rspec tests running

--- models/appointment.rb
+++ models/appointment.rb
@@ -6,4 +6,13 @@ class Appointment
   property :medic, String
   property :dateAndHour, DateTime
   property :duration, Integer
+  attr_accessor :medic, :dateAndHour, :duration
+
+  def Appointment.add_new_appointment(medic_name,date,hour,minutes,duration)
+    new_appointment=self.new
+    new_appointment.medic=medic_name
+    new_appointment.dateAndHour=DateTime.new(date.year,date.month,date.day,hour,minutes,0,0)
+    new_appointment.duration=duration
+    new_appointment
+  end
 end
--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -5,17 +5,18 @@ describe Appointment do
 
   describe 'add_new_appointment' do
     it "should create a new instance of an appointment with the right medic name" do
-      new_appointment=Appointment.add_new_appointment('Roberto Bolanios',DateTime.now.tomorrow,19,0,20)
-      new_appointment.medic.should eq 'RobertoBolanios'
+      new_appointment=Appointment.add_new_appointment('Roberto Bolanios',DateTime.now+1,19,0,20)
+      new_appointment.medic.should eq 'Roberto Bolanios'
     end
     it "should create a new instance of an appointment with the right date" do
-      entered_date_and_time = DateTime.now.tomorrow.change({:hour => 19, :min => 0, :sec => 0})
-      new_appointment=Appointment.add_new_appointment('Roberto Bolanios',DateTime.now.tomorrow,19,0,20)
+      now=DateTime.now
+      entered_date_and_time=DateTime.new(now.year,now.month,now.day+1,19,0,0,0)
+      new_appointment=Appointment.add_new_appointment('Roberto Bolanios',DateTime.now+1,19,0,20)
       new_appointment.dateAndHour.should eq entered_date_and_time
     end
     it "should create a new instance of an appointment with the right duration" do
-      new_appointment=Appointment.add_new_appointment('Roberto Bolanios',DateTime.now.tomorrow,19,0,20)
-      new_appointment.medic.should eq 20
+      new_appointment=Appointment.add_new_appointment('Roberto Bolanios',DateTime.now+1,19,0,20)
+      new_appointment.duration.should eq 20
     end
   end
 end
