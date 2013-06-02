7ffcb18330656910699fe50e2df566d118e0bb26
Facundo Mainere <fmmainere@gmail.com>
2013-06-02 17:20:00 -0300

    #13 - Model expected behaviour implemented with specs

--- models/appointment.rb
+++ models/appointment.rb
@@ -1,16 +1,42 @@
+require 'active_support/all'
 class Appointment
   include DataMapper::Resource
 
   # property <name>, <type>
   property :id, Serial
-  property :medic, String
-  property :dateAndHour, DateTime
-  property :duration, Integer
+  property :medic, String, :required => true
+  property :dateAndHour, DateTime, :required => true
+  property :duration, Integer, :required => true
   attr_accessor :medic, :dateAndHour, :duration
+
+  validates_with_method :check_date
+
+  def check_date
+    return (self.dateAndHour >= DateTime.now) if self.dateAndHour.is_a?(DateTime)
+    return false
+  end
+
+  def capitalize_name(name)
+    splitted=name.split(' ')
+    resultingName=''
+    splitted.each{|name| resultingName=resultingName+(name.capitalize)+' '}
+    resultingName[0..-2]
+  end
+
+  def check_turn_is_taken
+    turns_by_a_doctor=Appointment.all(:medic=>self.medic)
+    not turns_by_a_doctor.select{|appointment| self.overlaps(appointment)}.empty?
+  end
+
+  def overlaps(other_appointment)
+    first_app_range=self.dateAndHour..self.dateAndHour+self.duration.minutes
+    second_app_range=other_appointment.dateAndHour..other_appointment.dateAndHour+other_appointment.duration.minutes
+    (first_app_range.first <= second_app_range.last) and (second_app_range.first <= first_app_range.last)
+  end
 
   def Appointment.add_new_appointment(medic_name,date,hour,minutes,duration)
     new_appointment = self.new
-    new_appointment.medic = medic_name
+    new_appointment.medic = new_appointment.capitalize_name(medic_name)
     new_appointment.dateAndHour = DateTime.new(date.year,date.month,date.day,hour,minutes,0,0)
     new_appointment.duration = duration
     new_appointment
--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -19,4 +19,80 @@ describe Appointment do
       new_appointment.duration.should eq 20
     end
   end
+  describe 'check_date' do
+
+    it 'should return false if date is before today' do
+      appointment=Appointment.add_new_appointment('Roberto Bolanios',DateTime.now-1,19,0,20)
+      appointment.check_date.should be false
+    end
+
+    it 'should return false if date is today' do
+      appointment=Appointment.add_new_appointment('Roberto Bolanios',DateTime.now,19,0,20)
+      appointment.check_date.should be false
+    end
+
+    it 'should return true if date is after today' do
+      appointment=Appointment.add_new_appointment('Roberto Bolanios',DateTime.now+1,19,0,20)
+      appointment.check_date.should be true
+    end
+  end
+  describe 'capitalize_name' do
+    it 'should return the same name when name is already capitalized' do
+      appointment=Appointment.add_new_appointment('Roberto Bolanios',DateTime.now+1,19,0,20)
+      appointment.medic.should eq 'Roberto Bolanios'
+    end
+    it 'should return Roberto Bolanios when name is uppercase' do
+      appointment=Appointment.add_new_appointment('ROBERTO BOLANIOS',DateTime.now+1,19,0,20)
+      appointment.medic.should eq 'Roberto Bolanios'
+    end
+    it 'should return Roberto Bolanios when name is downcase' do
+      appointment=Appointment.add_new_appointment('roberto bolanios',DateTime.now+1,19,0,20)
+      appointment.medic.should eq 'Roberto Bolanios'
+    end
+    it 'should return Roberto Bolanios when name is mixed case' do
+      appointment=Appointment.add_new_appointment('RobertO BOlanIos',DateTime.now+1,19,0,20)
+      appointment.medic.should eq 'Roberto Bolanios'
+    end
+  end
+  describe 'overlaps' do
+    it 'should be true when appointments start at the same hour but ends at different hour' do
+      tomorrow=DateTime.now+1
+      appointment=Appointment.add_new_appointment('Roberto Bolanios',tomorrow,19,0,20)
+      new_appointment=Appointment.new
+      new_appointment.medic = 'Roberto Bolanios'
+      new_appointment.dateAndHour = DateTime.new(tomorrow.year,tomorrow.month,tomorrow.day,19,0,0,0)
+      new_appointment.duration = 30
+      new_appointment.overlaps(appointment).should be true
+    end
+
+    it 'should be true when an appointment time lapse is included in the other appointment time lapse' do
+      tomorrow=DateTime.now+1
+      appointment=Appointment.add_new_appointment('Roberto Bolanios',tomorrow,19,0,20)
+      new_appointment=Appointment.new
+      new_appointment.medic = 'Roberto Bolanios'
+      new_appointment.dateAndHour = DateTime.new(tomorrow.year,tomorrow.month,tomorrow.day,19,10,0,0)
+      new_appointment.duration = 5
+      new_appointment.overlaps(appointment).should be true
+    end
+
+    it 'should be true when appointments ends at the same hour but starts at different hour' do
+      tomorrow=DateTime.now+1
+      appointment=Appointment.add_new_appointment('Roberto Bolanios',tomorrow,19,0,20)
+      new_appointment=Appointment.new
+      new_appointment.medic = 'Roberto Bolanios'
+      new_appointment.dateAndHour = DateTime.new(tomorrow.year,tomorrow.month,tomorrow.day,19,10,0,0)
+      new_appointment.duration = 10
+      new_appointment.overlaps(appointment).should be true
+    end
+
+    it 'should be false when appointments time lapses doesnt include each other even partially' do
+      tomorrow=DateTime.now+1
+      appointment=Appointment.add_new_appointment('Roberto Bolanios',tomorrow,19,0,20)
+      new_appointment=Appointment.new
+      new_appointment.medic = 'Roberto Bolanios'
+      new_appointment.dateAndHour = DateTime.new(tomorrow.year,tomorrow.month,tomorrow.day,20,0,0,0)
+      new_appointment.duration = 30
+      new_appointment.overlaps(appointment).should be false
+    end
+  end
 end
